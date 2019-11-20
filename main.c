1a81abd542e7a26b8f8da045e4e1dac9a339f3b6
Ben-Gendler <57101218+Ben-Gendler@users.noreply.github.com>
2019-11-20 18:06:30 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "myBank.h"
+
+int main() {
+  firstoption = true;
+  char display_functions[] = "List of operations: \n\n O to open an account \n C to deactivate account \n P to print accounts \n B to get current account balance \n D to deposit to a balance \n W to withdraw from a 	balance
+  \n I to add interest to everyone \n E to remove all information and exit the program \n please choose an operation: \n";
+  if(first operation = true){
+  printf("%s",display_functions);
+  scanf("%c" , choose);}
+  choose = toupper(choose);}
+  else{
+    printf("choose another option: \n %c",display_functions);
+  }
+return 0;
+}

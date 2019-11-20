3b5ba5205a819ebea252fa1e0d3feaa493168352
RoyMash <57101692+RoyMash@users.noreply.github.com>
2019-11-20 13:42:35 +0200

    Update myBank.c
    To do : Open account 
                Print all the accounts and their balance.

--- myBank.c
+++ myBank.c
@@ -3,31 +3,94 @@
 
 bollean exit = false;
 
-while(false){
+while(!exit){
 	printf("List of operations: \n\n O to open an account \n C to deactivate account \n P to print accounts \n B to get current account balance \n D to deposit to a balance \n W to withdraw from a 	balance
-	"\n I to add interest to everyone \n E to remove all information and exit the program");
+	\n I to add interest to everyone \n E to remove all information and exit the program");
 	printf("Please choose an operation");
 	char choose;
 	scanf("%c" , choose);
 
-			// make small/big char ('O' -- 'o') 
+			// make small/big char ('O' -- 'o')
 
 
 	switch(choose){
 
 	case o:
 
-	open_account(); //O
-	deactivate_account(int acc_number); //C
-	print_accounts(); //P
-	get_balance(int acc_number); //B
-	deposit_to_balance(int acc_number, double amount); //D
-	withdraw_from_balance(int acc_number, double amount); //W
-	add_interest_to_all(double interest); //I
-	reset_all_and_exit(); //E
+		open_account(); //O
+
+	case c:
+
+		deactivate_account(int acc_number){  //C
+			if (accounts[acc_number - 901][1]) == 1){
+					accounts[acc_number - 901][0] = 0;
+					accounts[acc_number - 901][1] = 0;
+			}
+			else{
+				printf("The account number %d is already closed /n", acc_number);
+			}
+		}
+
+	case p:
+
+		print_accounts(); //P
+
+	case b:
+
+		get_balance(int acc_number){ //B
+
+			if (accounts[acc_number - 901][1]) == 1){
+					printf("Your balance is %lf\n", accounts[acc_number - 901][0]);
+			}
+			else{
+					printf("The account number %d is already closed /n", acc_number);
+			}
+		}
+
+	case d:
+
+	deposit_to_balance(int acc_number, double amount){ //D
+		if (accounts[acc_number - 901][1]) == 1){
+				accounts[acc_number - 901][0] = accounts[acc_number - 901][0] + amount;
+				printf("Your balance is %lf\n", accounts[acc_number - 901][0]);
+		}
+		else{
+				printf("The account number %d is closed /n", acc_number);
+			}
+	}
+  case w:
+
+	withdraw_from_balance(int acc_number, double amount){ //W
+		if (accounts[acc_number - 901][1]) == 1){
+			if (accounts[acc_number - 901][0] < amount) {
+				printf("Insufficient funds /n");
+			}
+			else{
+				accounts[acc_number - 901][0] = accounts[acc_number - 901][0] - amount;
+				printf("Your balance is %lf\n", accounts[acc_number - 901][0]);
+		}
+		else{
+				printf("The account number %d is closed /n", acc_number);
+			}
+	}
 
 
+  case i:
+
+		add_interest_to_all(double interest){ //I
+		for (int i = 0; i < 50; i++) {
+			accounts[i][0] = accounts[i][0] * ((interest/100)+1);
+		}
+		printf("Interest was updated for all accounts \n");
+	}
+	case e:
+
+		reset_all_and_exit(){ //E
+			for (int i = 0; i < 50; i++) {
+				accounts[i][0] = 0;
+				accounts[i][1] = 0;
+			}
+			printf("All of the accounts were removed. \n Thank you and goodbye.");
+			exit = true;
+		}
 }
-
-
-

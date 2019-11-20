e44c251551091e6d03ac4a6b486f0f83d295ab36
Ben-Gendler <57101218+Ben-Gendler@users.noreply.github.com>
2019-11-20 20:09:11 +0200

    Update myBank.h

--- myBank.h
+++ myBank.h
@@ -1,16 +1,14 @@
 #include "stdio.h"
-int main (){
-	int acounts [50][2] = { 0 };
-	return 0;
+
+	extern int accounts [50][2];
+
 }
 
-open_account(); //O
-deactivate_account(int acc_number); //C
-print_accounts(); //P
-get_balance(int acc_number); //B
-deposit_to_balance(int acc_number, double amount); //D
-withdraw_from_balance(int acc_number, double amount); //W
-add_interest_to_all(double interest); //I
-reset_all_and_exit(); //E
-
-
+void open_account(); //O
+void deactivate_account(int acc_number); //C
+void print_accounts(); //P
+void get_balance(int acc_number); //B
+void deposit_to_balance(int acc_number, double amount); //D
+void withdraw_from_balance(int acc_number, double amount); //W
+void add_interest_to_all(double interest); //I
+void reset_all_and_exit(); //E
